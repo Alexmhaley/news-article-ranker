f9c6870c4b0836ae10e1c42ea411d941c76cd4f0
Alexmhaley <alexanderhaley@gmail.com>
2019-06-22 13:30:54 +0100

    Adding broken files to commit will fix if I have the time

--- src/app/index.js
+++ src/app/index.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
+// import get_data from "endpoints.js";
+// var get_data = require('./endpoints.js');
 
-var get_data = require('./endpoints.js');
 
-class NewsCard extends React.Component {
-  render() {
-      const article = this.props.news_article;
-      const excerpt = article.excerpt;
-      console.log(article);
-      const ranking = 0;
-    return (
-      <div className="card">
-        <div id="info">
-            <h1>{title}</h1>
-            <p>{ranking}</p>
-        </div>
-      </div>
-    );
-  }
-}
 
 
 class CardList extends React.Component{
     render() {
-        const rows = []
-        this.props.articles.forEach((article) => {
-            rows.push(<NewsCard news_article={article} />)
-        });
+        const rows = [];
+        var articles = this.props.articles;
+        console.log(">>>>>>>>>>>>>>>>>>>>>>");
+        console.log(articles);
+        console.log(articles.length);
+        console.log(articles[1]);
+        console.log("(000000000000000000000)");
+        console.log(Array.isArray(articles));
+        for (var article in articles){
+            console.log(articles[article]);
+            // rows.push(<NewsCard news_article={article} />)
+        };
+
+        console.log("*******************");
         return(
           <table>
             <thead>
@@ -40,8 +36,43 @@ class CardList extends React.Component{
     }
 }
 
+class NewsCard extends React.Component {
+  render() {
+      const article = this.props.news_article;
+      console.log("))))))))))))))))))))))))))))");
+      console.log(article);
+      const title = article.title;
+      const ranking = 0;
+    return (
+      <div className="card">
+        <div id="info">
+            <h1>{title}</h1>
+            <p>{ranking}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+function get_data(){
+    var i;
+    var articles_promise =[];
+    var articles=[];
+    for (i = 1; i < 6; i++) {
+      articles_promise.push(axios.get('assets/article-' + i + '.json',  { crossdomain: true }).then(response => {return response.data}));
+  };
+  Promise.all(articles_promise).then(res => {
+    articles.push(res);
+    });
+  console.log("&&&&&&&&&&&&&&&&&&&&&");
+  console.log(JSON.stringify(articles));
+    return articles;
+}
 
 ReactDOM.render(
     <CardList articles={get_data()}/>,
   document.getElementById('container')
 );
+//articles.push({"data":response.data, "id":i})
