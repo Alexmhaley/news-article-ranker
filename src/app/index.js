e62a7d8cf7ebe83b370e4970d69c40f98e3b349b
Alexmhaley <alexanderhaley@gmail.com>
2019-06-22 10:34:40 +0100

    Updated to add get endpoint for the 3 articles

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,47 @@
-console.log("THIS ISN't THOUGH 234523");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var get_data = require('./endpoints.js');
+
+class NewsCard extends React.Component {
+  render() {
+      const article = this.props.news_article;
+      const excerpt = article.excerpt;
+      console.log(article);
+      const ranking = 0;
+    return (
+      <div className="card">
+        <div id="info">
+            <h1>{title}</h1>
+            <p>{ranking}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class CardList extends React.Component{
+    render() {
+        const rows = []
+        this.props.articles.forEach((article) => {
+            rows.push(<NewsCard news_article={article} />)
+        });
+        return(
+          <table>
+            <thead>
+              <tr>
+                <th>News Articles</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <CardList articles={get_data()}/>,
+  document.getElementById('container')
+);
